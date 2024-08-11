6f412efbb875ccc3c546c3da08a8cf8f55dfb172
Prathamesh Kalekar <kalekarprathamesh19@gmail.com>
2024-08-12 00:26:07 +0530

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from "tailwindcss";
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/dream-ahead-frontend/",
   plugins: [react()],
   css: {
     postcss: {
